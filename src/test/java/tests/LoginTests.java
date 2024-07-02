b587873ae1b99f704bb8fe86a198bd7e8a9d2a90
Bigboss2506 <belyak2506@gmail.com>
2024-07-02 20:35:28 +0300

    loginSuccses

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,25 @@
 package tests;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
-
+public class LoginTests extends TestBase {
 
     @Test
-    public void test(){
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogget());
 
     }
+
+
 }
