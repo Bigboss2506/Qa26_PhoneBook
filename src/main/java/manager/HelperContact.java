4001dc3c488d62687440ac44c949ee3b1cb58332
Bigboss2506 <belyak2506@gmail.com>
2024-07-27 02:42:10 +0300

    add listenerWD

--- src/main/java/manager/HelperContact.java
+++ src/main/java/manager/HelperContact.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.Random;
 
 public class HelperContact extends HelperBase{
     public HelperContact(WebDriver wd) {
@@ -56,5 +57,57 @@ public class HelperContact extends HelperBase{
 
     public boolean isAddPageStillDisplayed() {
         return isElementPresent(By.cssSelector("a.active[href='/add']"));
+    }
+
+    public int removeOneContact() {
+        int  before = counterOfContacts();
+        logger.info("Number of Contacts before remove is --->" + before);
+        removeContact();
+        int after = counterOfContacts();
+        logger.info("Number of Contacts after remove is --->" + after);
+
+
+        return before - after;
+    }
+
+    private void removeContact() {
+        click(By.cssSelector(".contact-item_card__2SOIM"));
+        click(By.xpath("//button[text()='Remove']"));
+        pause(500);
+    }
+
+    private int counterOfContacts() {
+        return wd.findElements(By.cssSelector(".contact-item_card__2SOIM")).size();
+    }
+
+    public void removeAllContacts() {
+        while (counterOfContacts() != 0){
+            removeContact();
+        }
+    }
+
+    public void provideContacts() {
+        if(counterOfContacts() < 3){
+            for (int i = 0; i < 3; i++){
+                addOneContact();
+            }
+        }
+    }
+
+    private void addOneContact() {
+        int i = new Random().nextInt(1000)+1000;
+        Contact contact = Contact.builder()
+                .name("Harry" + i)
+                .lastName("Potter")
+                .email("harry" + i + "@gmail.com")
+                .address("Hogwards")
+                .phone("55566777" + i)
+                .description("Friend")
+                .build();
+
+        openContactForm();
+        fillContactForm(contact);
+        saveContact();
+        pause(500);
     }
 }
--- src/test/java/tests/RemoveContactTests.java
+++ src/test/java/tests/RemoveContactTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import models.User;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,17 +15,21 @@ public class RemoveContactTests extends TestBase{
             app.getHelperUser().login(new User().setEmail("jeka@gmail.com").setPassword("Bigboss2506$"));
         }
 
-      //  app.getHelperContact().provideContacts();
+        app.getHelperContact().provideContacts();
 
     }
 
 
     @Test
     public void removeFirstContact(){
+        Assert.assertEquals(app.getHelperContact().removeOneContact(), 1);
 
     }
     @Test
     public void removeAllContacts(){
+        app.getHelperContact().removeAllContacts();
+        Assert.assertTrue(app.getHelperUser().isNoContactsDisplayed());
+
 
     }
 }
